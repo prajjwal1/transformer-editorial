8eb50d40d593ac2c94ef0abc857748ede0644ae9
prajjwal1 <prajjwalin@protonmail.com>
2020-12-04 12:05:25 +0900

    upload

--- run_edit.py
+++ run_edit.py
@@ -1,4 +1,5 @@
 import random
+import os
 import json
 from dataclasses import dataclass, field
 from typing import Optional
@@ -45,11 +46,14 @@ class ModelArguments:
 class DataArguments:
     fold: int = field()
     k: int = field()
+    context: str = field()
+    max_length: int = field()
+    data_path: str = field()
 
 parser = HfArgumentParser((ModelArguments, DataArguments, TrainingArguments))
 model_args, data_args, training_args = parser.parse_args_into_dataclasses()
 
-with open("data/edit_data.json", "r") as read_file:
+with open(os.path.join(data_args.data_path,"editorial_main.json"), "r") as read_file:
     data = json.load(read_file)
 
 df = pd.DataFrame(data)
@@ -57,23 +61,22 @@ df = pd.DataFrame(data)
 tokenizer = AutoTokenizer.from_pretrained(model_args.model_name_or_path)
 
 def preprocess_function(examples):
-    #  prompt = str(examples['body'])
-    #  prompt = str(examples['abstract'])
-    prompt = str(examples['headline'])
-    choice0, choice1, choice2 = str(examples['thesis']), str(examples['anti-thesis']), str(examples['third-option'])
-    choices = [choice0, choice1, choice2]
+    prompt = str(examples[data_args.context])
+    choice_0, choice_1, choice_2 = str(examples['thesis']), str(examples['anti-thesis']), str(examples['third-option'])
+    choice_3 = str(examples['neg-thesis'])
+    choices = [choice_0, choice_1, choice_2, choice_3]
     random.shuffle(choices)
     # Specify max_length in encoding when not using body
-    encoding = tokenizer([prompt, prompt, prompt], choices, return_tensors='pt', max_length=512, padding='max_length', truncation=True)
-    encoding["label"] = choices.index(choice0)
+    encoding = tokenizer([prompt, prompt, prompt, prompt], choices, return_tensors='pt', max_length=data_args.max_length, padding='max_length', truncation=True)
+    encoding["label"] = choices.index(choice_0)
     return encoding
 
 
-five_fold = {0: list(range(0, 169)),
-             1: list(range(169, 338)),
-             2: list(range(338, 507)),
-             3: list(range(507, 676)),
-             4: list(range(676, 845))}
+five_fold = {0: list(range(0, 168)),
+             1: list(range(168, 336)),
+             2: list(range(336, 504)),
+             3: list(range(504, 672)),
+             4: list(range(672, 840))}
 
 ten_fold = {0: list(range(0, 84)),
             1: list(range(84, 168)),
@@ -87,20 +90,20 @@ ten_fold = {0: list(range(0, 84)),
             9: list(range(756, 845))}
 
 if data_args.fold == 5:
-    total_indexes = set(range(0, 845))
+    total_indexes = set(range(0, len(df)))
     valid_indexes = set(five_fold[data_args.k])
     train_indexes = total_indexes - valid_indexes
     train_indexes, valid_indexes = list(train_indexes), list(valid_indexes)
-    assert len(train_indexes)+len(valid_indexes) <= 845
-    assert len(train_indexes)+len(valid_indexes) > 840
+    assert len(train_indexes)+len(valid_indexes) <= len(df)
+    assert len(train_indexes)+len(valid_indexes) > len(df)-5
 
-if data_args.fold == 10:
-    total_indexes = set(range(0, 845))
-    valid_indexes = set(ten_fold[data_args.k])
-    train_indexes = total_indexes - valid_indexes
-    train_indexes, valid_indexes = list(train_indexes), list(valid_indexes)
-    assert len(train_indexes)+len(valid_indexes) <= 845
-    assert len(train_indexes)+len(valid_indexes) > 840
+#  if data_args.fold == 10  :
+    #  total_indexes = set(range(0, 845))
+    #  valid_indexes = set(ten_fold[data_args.k])
+    #  train_indexes = total_indexes - valid_indexes
+    #  train_indexes, valid_indexes = list(train_indexes), list(valid_indexes)
+    #  assert len(train_indexes)+len(valid_indexes) <= 845
+    #  assert len(train_indexes)+len(valid_indexes) > 840
 
 
 train_dataset = Dataset.from_pandas(df.iloc[train_indexes])
