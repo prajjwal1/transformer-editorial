2066a2a7296c9a7de67739086c24e15f5556364d
prajjwal1 <prajjwalin@protonmail.com>
2021-01-07 15:42:40 +0900

    update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # transformer-editorial
+
+
+
+```
+$ python3 run_edit.py --model_name_or_path allenai/longformer-base-4096 --output_dir /home/nlp/experiments/edit --per_device_train_batch_size 8 --per_device_eval_batch_size 16 --do_eval --do_train --evaluation_strategy epoch --num_train_epochs 3 --k 2 --fold 5
+```
--- run_glue.sh
+++ run_glue.sh
@@ -1,1 +1,5 @@
-for k in 0 1 2 3 4; do python3 run_edit.py --output_dir /home/nlp/experiments/edit_synthetic/third_option/lf_abstract/$k --per_device_train_batch_size 4 --per_device_eval_batch_size 4 --do_eval --do_train --evaluation_strategy epoch --num_train_epochs 5 --model_name_or_path allenai/longformer-base-4096 --k $k --fold 5 --data_path data/synthetic_neg.json --context abstract --max_length 128 --overwrite_output_dir; done
+export CONTEXT=headline
+export BS=4
+export MAX_LENGTH=128
+
+for k in 0 1 2 3 4; do python3 run_edit.py --output_dir /home/nlp/experiments/edit_synthetic/thesis/"lf_"$CONTEXT/$k --per_device_train_batch_size $BS --per_device_eval_batch_size $BS --do_eval --do_train --evaluation_strategy epoch --num_train_epochs 5 --model_name_or_path allenai/longformer-base-4096 --k $k --fold 5 --data_path data/synthetic_thesis.json --context $CONTEXT --max_length $MAX_LENGTH --overwrite_output_dir; done
